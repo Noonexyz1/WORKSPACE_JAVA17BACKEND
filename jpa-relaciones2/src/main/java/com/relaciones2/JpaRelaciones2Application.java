aee7e24c192d05f7977e9f4646f9cb735e3e7cab
Noonexyz1 <aries-d2github@hotmail.com>
2024-01-31 17:43:10 -0400

    jpa-relaciones2: proyecto inicializado

--- jpa-relaciones2/src/main/java/com/relaciones2/JpaRelaciones2Application.java
+++ jpa-relaciones2/src/main/java/com/relaciones2/JpaRelaciones2Application.java
@@ -1,4 +1,4 @@
-package com.relaciones;
+package com.relaciones2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- jpa-relaciones2/src/test/java/com/relaciones2/JpaRelaciones2ApplicationTests.java
+++ jpa-relaciones2/src/test/java/com/relaciones2/JpaRelaciones2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.relaciones;
+package com.relaciones2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
