011b0e79bd8265b4eb105b2f5d60fa14d2dc9314
Noonexyz1 <aries-d2github@hotmail.com>
2024-01-31 17:33:39 -0400

    jpa-relaciones2: proyecto inicializado

---
+++ jpa-relaciones2/src/main/java/com/relaciones/JpaRelaciones2Application.java
@@ -0,0 +1,13 @@
+package com.relaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelaciones2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRelaciones2Application.class, args);
+	}
+
+}
